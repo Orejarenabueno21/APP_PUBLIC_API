5623a0ed08cc01515c934878189ab152ddd97e7b
raul <orejarena1999@gmail.com>
2023-05-27 21:12:03 -0500

    ejemplo api Rick and morty

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -1,19 +1,41 @@
 import {React, useState} from 'react'
 
 const PeticionApi = () => {
-    const [contador, setContador]= useState(0)
-    const aumentar = () => {
-        setContador(contador+1)
+    const [personajes, setPersonajes]= useState ([])
+    const [paginacion, setPaginacion]= useState (0)
+
+    const obtenerPersonaje = async () => {
+        try {
+            const res = await  fetch(`https://rickandmortyapi.com/api/character/?page=${paginacion}`)
+            const {results}=await res.json()
+            setPersonajes(results)
+        }catch(error){
+            console.log(error)
+        }
+        
     }
-    const disminuir = () => {
-        setContador(contador-1)
+    const Siguiente =  () => {
+        setPaginacion(paginacion+1)
+        obtenerPersonaje()
+    }
+    const Atras =  () => {
+        setPaginacion(paginacion-1)
+        obtenerPersonaje()
     }
   return (
     <div>
-        <h1>Peticion Del Api Marvel</h1>
-        <button onClick={disminuir}>Resta</button>
-        <span> {contador} </span>
-        <button onClick={aumentar}>Suma</button>
+        <h1>Peticion Del Api</h1>
+        <button onClick={obtenerPersonaje}>Traer Personajes </button>
+        <button onClick={Atras}>Atras</button> 
+        <button onClick={Siguiente}>Siguiente</button>
+        {   
+            personajes.map(({id, name, image}) => (
+                <div key={id}>
+                    <h4>{id} - {name}</h4>
+                    <img src={image} alt={name} />
+                </div>
+            ))
+        }
     </div>
   )
 }
