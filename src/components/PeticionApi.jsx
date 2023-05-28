9e522c1696ecebd1a0cf3e5afbff21cd9e24b3dd
raul <orejarena1999@gmail.com>
2023-05-28 15:57:12 -0500

    consulta

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -3,10 +3,11 @@ import {React, useState} from 'react'
 const PeticionApi = () => {
     const [personajes, setPersonajes]= useState ([])
     const [paginacion, setPaginacion]= useState (0)
+    const [ide, setIde]= useState (1)
 
     const obtenerPersonaje = async () => {
         try {
-            const res = await  fetch(`https://rickandmortyapi.com/api/character/?page=${paginacion}`)
+            const res = await  fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1&offset=${paginacion}`)
             const {results}=await res.json()
             setPersonajes(results)
         }catch(error){
@@ -15,13 +16,16 @@ const PeticionApi = () => {
         
     }
     const Siguiente =  () => {
+        setIde(ide+1)
         setPaginacion(paginacion+1)
         obtenerPersonaje()
     }
     const Atras =  () => {
+        setIde(ide-1)
         setPaginacion(paginacion-1)
         obtenerPersonaje()
     }
+    const imagenes =["https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/1.png","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/2.png","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/back/3.png"]
   return (
     <div>
         <h1>Peticion Del Api</h1>
@@ -29,10 +33,10 @@ const PeticionApi = () => {
         <button onClick={Atras}>Atras</button> 
         <button onClick={Siguiente}>Siguiente</button>
         {   
-            personajes.map(({id, name, image}) => (
-                <div key={id}>
-                    <h4>{id} - {name}</h4>
-                    <img src={image} alt={name} />
+            personajes.map(({id, name}) => (
+                <div>
+                    <h4>{ide} - {name}</h4>
+                    <img src={imagenes[paginacion]} alt={name} />
                 </div>
             ))
         }
